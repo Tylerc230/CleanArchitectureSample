4ece62b88a915bcd9134ef6230aba083f71c40f5
Tyler Casselman <tylerc230@gmail.com>
2017-11-20 20:18:46 -0800

    Initial commit

---
+++ CleanArchitectureSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CleanArchitectureSample
+//
+//  Created by Tyler Casselman on 11/20/17.
+//  Copyright © 2017 Tyler Casselman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
