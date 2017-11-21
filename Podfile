cb112d0bda53949e4fa31b7c7c9ddf7f11a10f06
Tyler Casselman <tylerc230@gmail.com>
2017-11-20 21:44:50 -0800

    Add Quick and Nimble to both test targets
    We have 2 test targets one macOS cli and the other iOS.

--- Podfile
+++ Podfile
@@ -1,4 +1,11 @@
-platform :ios, '11.0'
-
-pod 'Nimble'
-pod 'Quick'
+use_frameworks!
+target 'CleanArchitectureSampleTests' do
+  platform :ios, '11.1'
+  pod 'Nimble'
+  pod 'Quick'
+end
+target 'CleanArchitectureCLITests' do
+  platform :macos, '10.13'
+  pod 'Nimble'
+  pod 'Quick'
+end
--- time.sh
+++ time.sh
@@ -1,8 +1,8 @@
 echo "iOS tests clean"
-time xcodebuild clean test -quiet -destination 'platform=iOS Simulator,name=iPhone 7,OS=11.1' -scheme CleanArchitectureSample
+time xcodebuild clean test -quiet -destination 'platform=iOS Simulator,name=iPhone 7,OS=11.1' -scheme CleanArchitectureSample -workspace CleanArchitectureSample.xcworkspace
 echo "incremental"
-time xcodebuild test -quiet -destination 'platform=iOS Simulator,name=iPhone 7,OS=11.1' -scheme CleanArchitectureSample
+time xcodebuild test -quiet -destination 'platform=iOS Simulator,name=iPhone 7,OS=11.1' -scheme CleanArchitectureSample -workspace CleanArchitectureSample.xcworkspace
 echo "macOS tests"
-time xcodebuild clean test  -quiet -destination 'platform=macOS' -scheme CleanArchitectureCLI
+time xcodebuild clean test  -quiet -destination 'platform=macOS' -scheme CleanArchitectureCLI -workspace CleanArchitectureSample.xcworkspace
 echo "incremental"
-time xcodebuild test  -quiet -destination 'platform=macOS' -scheme CleanArchitectureCLI
+time xcodebuild test  -quiet -destination 'platform=macOS' -scheme CleanArchitectureCLI -workspace CleanArchitectureSample.xcworkspace
